cec6efd0ea77fef76ba7e44259b6b003737f2e40
Sandeep Ramavana <zikationindia@gmail.com>
2026-01-12 19:09:05 +0530

    Fix single quote in HTML

--- pages/404.js
+++ pages/404.js
@@ -6,7 +6,7 @@ export default function Custom404() {
         <FullScreenOverlay showClose={false}>
             <div className="Error404Content">
                 <p>Oops! How did you land here?</p>
-                <p>The link you clicked/touched is wrong. It's our issue. Sorry!</p>
+                <p>The link you clicked/touched is wrong. It&apos;s our issue. Sorry!</p>
                 <p>Or you may have typed a wrong URL. If yes, you can double check the same.</p>
                 <p>In any case, you can search for tours in our homepage.</p>
                 <Link href='/'><strong>Go to Homepage</strong></Link>
