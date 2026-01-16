ca0c6b17ec50e74227b422df82e51cae34c2cd50
Sandeep Ramavana <zikationindia@gmail.com>
2026-01-16 16:18:14 +0530

    Routing changes / validation

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,18 @@
+import backend from '@/utils/backend'
+
+export async function getServerSideProps({ res }) {
+    const response = await fetch(`${backend.sitemaptoursURL}`)
+    const xml = await response.text();
+
+    res.setHeader("Content-Type", "text/xml; charset=utf-8");
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("Cache-Control", "public, max-age=3600");
+    res.write(xml);
+    res.end();
+  
+    return { props: {} };
+  }
+  
+  export default function Sitemap() {
+    return null;
+  }
