d8b5f38eb4e3186da7484347510616066676c2cb
Sandeep Ramavana <zikationindia@gmail.com>
2026-03-27 20:58:13 +0530

    Fix the import of RedirectOnError

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,4 +1,4 @@
-import RedirectOnError from '@/travel-components/Error/RedirectOnError'
+import RedirectOnError from "@/components/common/Error/RedirectOnError"
 
 export default function SearchResults() {
     const str = "You haven't specified any place to search"
