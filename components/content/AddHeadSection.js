cb8ee4b6c5a42300ceff343b66bda939e030e581
Sandeep Ramavana <zikationindia@gmail.com>
2026-03-29 13:13:00 +0530

    test with jpg

--- components/content/AddHeadSection.js
+++ components/content/AddHeadSection.js
@@ -8,7 +8,8 @@ const AddHeadSection = ({item, skip = false}) => {
         `${SiteBaseURL}/${item.location}/${item.sublocation}/${item.slug}`
     const canonicalKeywords = item.variantKeywords && Array.isArray(item.variantKeywords) ? item.variantKeywords.join(', ') : null
     const seotitle = item.seotitle ? `${item.seotitle} - ${BrandName}` : `${item.title} - ${BrandName}`
-    const image = `${SiteBaseURL}${item.bkgd?.path}` || `${SiteBaseURL}${DefaultTourImage}`
+    // const image = `${SiteBaseURL}${item.bkgd?.path}` || `${SiteBaseURL}${DefaultTourImage}`
+    const image = `${SiteBaseURL}/home/bhutan.jpg`
 
     return (
         <Head>
